e2f69339404f3d63c2355b221adc6b028396c092
adnroboticsfr <adnroboticsfr@gmail.com>
2024-10-17 18:57:39 +0200

    Update setup.py

--- smartpi_dht/setup.py
+++ smartpi_dht/setup.py
@@ -3,13 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="smartpi_dht",
     version="0.1.0",
-    description="A package to read DHT11/DHT22 temperature and humidity sensors on Smart Pi One.",
-    author="Your Name",
-    author_email="your.email@example.com",
+    description="A package to read DHT11/DHT22 temperature and humidity sensors without external libraries.",
+    author="ADNRoboticsfr",
+    author_email="adnroboticsfr@gmail.com",
     packages=find_packages(),
-    install_requires=[
-        "Adafruit_DHT",
-    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
